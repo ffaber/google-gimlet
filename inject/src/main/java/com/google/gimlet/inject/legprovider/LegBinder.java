cb988f57bb4f338ca3dc6e6d64b696c952ed1d46
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-05-08 14:34:36 +0000

    Initial LegModuleBuilder commit.

---
+++ inject/src/main/java/com/google/gimlet/inject/legprovider/LegBinder.java
@@ -0,0 +1,160 @@
+// Copyright 2011 Google Inc.  All Rights Reserved
+
+package com.google.gimlet.inject.legprovider;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.inject.Binder;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.InjectionPoint;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class is very loosely based on {@code FactoryProvider2}.
+ * <p>
+ * Its main functionality is how its {@link #bindTo(Key)} method returns a
+ * {@link Module module} that binds a given {@link TypeLiteral} to a
+ * particular profile of a target class.  This "profile" is defined by the
+ * set of values given as {@link LabeledKey}s, each of which corresponds to
+ * a constructor parameter on the target class that is annotated with
+ * {@link Leg}.
+ *
+ */
+class LegBinder<T> {
+
+  private final Constructor constructor;
+  private final TypeLiteral<? extends T> implementationType;
+  private final Map<Key<?>, Key<?>> configKeyToValueKeyMap;
+
+  LegBinder(
+      TypeLiteral<? extends T> implementationType,
+      Set<LabeledKey> labeledValueKeys) {
+    this.implementationType = implementationType;
+
+    // TODO(amatveev): Consider implementing support for AssistedInject
+    // constructors. The challenge lies in writing code that will find matching
+    // constructors based on types and labels.
+    // Find a matching constructor
+    InjectionPoint ctorInjectionPoint =
+        InjectionPoint.forConstructorOf(implementationType);
+    this.constructor = (Constructor) checkNotNull(
+        ctorInjectionPoint.getMember(),
+        "No suitable constructor was found in %s", implementationType);
+
+    // We need to create some configuration keys (aka keys that appear on the
+    // constructor of the implementation class).
+    this.configKeyToValueKeyMap =
+        mapConfigKeysToValueKeys(constructor, labeledValueKeys);
+  }
+
+  /**
+   * This method maps the {@link Key keys} given to the {@code using()} method
+   * of {@link LegModuleBuilder} to the {@link Assisted} parameters on the
+   * constructor of the target class.
+   */
+  private Map<Key<?>, Key<?>> mapConfigKeysToValueKeys(
+      Constructor constructor, Set<LabeledKey> labeledValueKeys) {
+    Type[] paramTypes = constructor.getGenericParameterTypes();
+    Class<?>[] paramClasses = constructor.getParameterTypes();
+    Annotation[][] paramAnnotations = constructor.getParameterAnnotations();
+
+    Map<Key<?>, Key<?>> configKeyToValueKeyMap = Maps.newHashMap();
+
+    // Extract the configuration keys
+    Map<LabeledKey, Key<?>> configLabeledKeyToConfigKeyMap = Maps.newHashMap();
+    for (int i = 0; i < paramTypes.length; i++) {
+      for (int j = 0; j < paramAnnotations[i].length; j++) {
+        if (paramAnnotations[i][j].annotationType().equals(Leg.class)) {
+          // If the configuration type is a Provider, then we must make sure
+          // to use the type of the Provider instead
+          final TypeLiteral<?> configTypeLiteral;
+          if (paramClasses[i].equals(Provider.class)
+              || paramClasses[i].equals(javax.inject.Provider.class)) {
+            checkState(
+                paramTypes[i] instanceof ParameterizedType,
+                "%s should be a ParameterizedType, but instead is %s",
+                paramClasses[i],
+                paramTypes[i]);
+            ParameterizedType paramType = (ParameterizedType) paramTypes[i];
+            configTypeLiteral = TypeLiteral.get(getOnlyElement(
+                Arrays.asList(paramType.getActualTypeArguments())));
+          } else {
+            configTypeLiteral = TypeLiteral.get(paramTypes[i]);
+          }
+
+          Leg annotation = (Leg) paramAnnotations[i][j];
+          configLabeledKeyToConfigKeyMap.put(
+              LabeledKey.of(Key.get(configTypeLiteral), annotation.value()),
+              Key.get(configTypeLiteral, annotation));
+        }
+      }
+    }
+
+    // Now pair up configuration keys with value keys
+    Set<Key<?>> configKeysForLogging =
+        ImmutableSet.copyOf(configLabeledKeyToConfigKeyMap.values());
+
+    checkState(
+        configKeysForLogging.size() == labeledValueKeys.size(),
+        "The number of value keys does not match the number of configuration "
+        + "keys. Number of value keys: %s Number of configuration keys: %s",
+        configKeysForLogging.size(), labeledValueKeys.size());
+
+    for (LabeledKey labeledValueKey : labeledValueKeys) {
+      Key<?> valueKey = labeledValueKey.getKey();
+      LabeledKey configLookupKey = LabeledKey.of(
+          Key.get(valueKey.getTypeLiteral()), labeledValueKey.getLabel());
+
+      Key<?> configKey = checkNotNull(
+          configLabeledKeyToConfigKeyMap.get(configLookupKey),
+          "Value key %s does not have a corresponding configuration key in %s",
+          labeledValueKey, configKeysForLogging);
+      configLabeledKeyToConfigKeyMap.remove(configLookupKey);
+
+      configKeyToValueKeyMap.put(configKey, valueKey);
+    }
+
+    checkState(
+        configLabeledKeyToConfigKeyMap.isEmpty(),
+        "Config keys %s did not have a matching value key in %s",
+        configLabeledKeyToConfigKeyMap.values(),
+        labeledValueKeys);
+
+    return configKeyToValueKeyMap;
+  }
+
+  Module bindTo(final Key<T> returnType) {
+    return new PrivateModule() {
+      @SuppressWarnings({ "unchecked", "RedundantCast" })
+      // raw keys are necessary for the args array and return value
+      @Override protected void configure() {
+        Binder binder = binder();
+
+        for (Key<?> configKey : configKeyToValueKeyMap.keySet()) {
+          binder.bind(configKey)
+              .to((Key) configKeyToValueKeyMap.get(configKey));
+        }
+
+        binder.bind(returnType)
+            .toConstructor(constructor, (TypeLiteral) implementationType);
+
+        expose(returnType);
+      }
+    };
+  }
+}
---
+++ inject/src/test/java/com/google/gimlet/inject/legprovider/LegModuleBuilderTest.java
@@ -0,0 +1,333 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.gimlet.inject.legprovider;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.BindingAnnotation;
+import com.google.inject.ConfigurationException;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+
+import junit.framework.TestCase;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Tests the {@link LegModuleBuilderTest} class.
+ *
+ */
+public class LegModuleBuilderTest extends TestCase {
+
+  private interface OneConfigurableParamInterface {
+    Double getNonConfigurableParam();
+    Integer getConfigurableParam();
+  }
+
+  // Classes below all have a non-configurable parameter injected into them.
+  // This is done purposely in order to test that non configurable components
+  // are injected normally in all cases.
+  private static class OneConfigurableParam
+      implements OneConfigurableParamInterface {
+
+    Double nonConfigurableParam;
+    Integer configurableParam;
+
+    @Inject
+    OneConfigurableParam(
+        Double nonConfigurableParam,
+        @Leg Integer configurableParam) {
+      this.nonConfigurableParam = nonConfigurableParam;
+      this.configurableParam = configurableParam;
+    }
+
+    @Override
+    public Double getNonConfigurableParam() {
+      return nonConfigurableParam;
+    }
+
+    @Override
+    public Integer getConfigurableParam() {
+      return configurableParam;
+    }
+  }
+
+  private static class TwoConfigurableParams {
+
+    Double nonConfigurableParam;
+    Integer configurableParam1;
+    String configurableParam2;
+
+    @Inject TwoConfigurableParams(
+        Double nonConfigurableParam,
+        @Leg Integer configurableParam1,
+        @Leg String configurableParam2) {
+      this.nonConfigurableParam = nonConfigurableParam;
+      this.configurableParam1 = configurableParam1;
+      this.configurableParam2 = configurableParam2;
+    }
+  }
+
+  private static class TwoConfigurableParamsOfSameType {
+
+    Double nonConfigurableParam;
+    Integer configurableParam1;
+    Integer configurableParam2;
+
+    @Inject TwoConfigurableParamsOfSameType(
+        Double nonConfigurableParam,
+        @Leg("one") Integer configurableParam1,
+        @Leg("otherone") Integer configurableParam2) {
+      this.nonConfigurableParam = nonConfigurableParam;
+      this.configurableParam1 = configurableParam1;
+      this.configurableParam2 = configurableParam2;
+    }
+  }
+
+  private static class ConfigurableParameterThroughProvider {
+
+    Double nonConfigurableParam;
+    Integer configurableParam;
+
+    @Inject ConfigurableParameterThroughProvider(
+        Double nonConfigurableParam,
+        @Leg Provider<Integer> configurableParamProvider) {
+      this.nonConfigurableParam = nonConfigurableParam;
+      this.configurableParam = configurableParamProvider.get();
+    }
+  }
+
+  private static class MoreThanOneConstructorMarkedWithInject {
+
+    @Inject MoreThanOneConstructorMarkedWithInject(
+        @Leg Integer configurableParam) {}
+
+    @Inject MoreThanOneConstructorMarkedWithInject(
+        @Leg String configurableParam) {}
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target({ElementType.FIELD, ElementType.PARAMETER})
+  @BindingAnnotation
+  private @interface IsAnotherInteger {}
+
+  private static final Integer INTEGER_VALUE = 0;
+  private static final Integer ANOTHER_INTEGER_VALUE = 1;
+  private static final String STRING_VALUE = "This is a string value";
+  private static final Double DOUBLE_VALUE = 0.1;
+
+  private static final Key<Integer> INTEGER_KEY = Key.get(Integer.class);
+  private static final Key<Integer> ANOTHER_INTEGER_KEY =
+      Key.get(Integer.class, IsAnotherInteger.class);
+  private static final Key<String> STRING_KEY = Key.get(String.class);
+
+  // This module provides test bindings, whose keys can be used as input for
+  // the configurable parameters of test classes.
+  private static final Module SEED_MODULE = new AbstractModule() {
+    @Override
+    protected void configure() {
+      bind(INTEGER_KEY).toInstance(INTEGER_VALUE);
+      bind(ANOTHER_INTEGER_KEY).toInstance(ANOTHER_INTEGER_VALUE);
+      bind(STRING_KEY).toInstance(STRING_VALUE);
+      bind(Double.class).toInstance(DOUBLE_VALUE);
+    }
+  };
+
+  public void testOneConfigurableParam() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<OneConfigurableParam>()
+            .implement(OneConfigurableParam.class)
+            .using(INTEGER_KEY)
+            .build());
+    OneConfigurableParam instance =
+        injector.getInstance(OneConfigurableParam.class);
+
+    assertEquals(DOUBLE_VALUE, instance.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance.configurableParam);
+  }
+
+  public void testOneConfigurableParam_multipleConfigurations() {
+    Named named1 = Names.named("1");
+    Named named2 = Names.named("2");
+
+    Injector injector = getInjector(
+        new LegModuleBuilder<OneConfigurableParam>()
+            .implement(OneConfigurableParam.class)
+            .using(INTEGER_KEY)
+            .build(named1),
+        new LegModuleBuilder<OneConfigurableParam>()
+            .implement(OneConfigurableParam.class)
+            .using(INTEGER_KEY)
+            .build(named2));
+
+    OneConfigurableParam instance1 =
+        injector.getInstance(Key.get(OneConfigurableParam.class, named1));
+    assertEquals(DOUBLE_VALUE, instance1.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance1.configurableParam);
+
+    OneConfigurableParam instance2 =
+        injector.getInstance(Key.get(OneConfigurableParam.class, named2));
+    assertEquals(DOUBLE_VALUE, instance2.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance2.configurableParam);
+  }
+
+  public void testTwoConfigurableParams() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<TwoConfigurableParams>()
+            .implement(TwoConfigurableParams.class)
+            .using(INTEGER_KEY)
+            .using(STRING_KEY)
+            .build());
+    TwoConfigurableParams instance =
+        injector.getInstance(TwoConfigurableParams.class);
+
+    assertEquals(DOUBLE_VALUE, instance.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance.configurableParam1);
+    assertEquals(STRING_VALUE, instance.configurableParam2);
+  }
+
+  public void testTwoConfigurableParamsOfSameType() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<TwoConfigurableParamsOfSameType>()
+            .implement(TwoConfigurableParamsOfSameType.class)
+            .using(INTEGER_KEY, "one")
+            .using(ANOTHER_INTEGER_KEY, "otherone")
+            .build());
+    TwoConfigurableParamsOfSameType instance =
+        injector.getInstance(TwoConfigurableParamsOfSameType.class);
+
+    assertEquals(DOUBLE_VALUE, instance.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance.configurableParam1);
+    assertEquals(ANOTHER_INTEGER_VALUE, instance.configurableParam2);
+  }
+
+  public void testConfigurableParameterThroughProvider() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<ConfigurableParameterThroughProvider>()
+            .implement(ConfigurableParameterThroughProvider.class)
+            .using(INTEGER_KEY)
+            .build());
+    ConfigurableParameterThroughProvider instance =
+        injector.getInstance(ConfigurableParameterThroughProvider.class);
+
+    assertEquals(DOUBLE_VALUE, instance.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance.configurableParam);
+  }
+
+  public void testBindingInterfaceToConfigurableClass() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<OneConfigurableParamInterface>()
+            .implement(
+                OneConfigurableParamInterface.class, OneConfigurableParam.class)
+            .using(INTEGER_KEY)
+            .build());
+    OneConfigurableParamInterface instance =
+        injector.getInstance(OneConfigurableParamInterface.class);
+
+    assertEquals(DOUBLE_VALUE, instance.getNonConfigurableParam());
+    assertEquals(INTEGER_VALUE, instance.getConfigurableParam());
+  }
+
+  public void testConfigurationKeysInMixedOrder() {
+    Injector injector = getInjector(
+        new LegModuleBuilder<TwoConfigurableParams>()
+            .implement(TwoConfigurableParams.class)
+            .using(STRING_KEY)
+            .using(INTEGER_KEY)
+            .build());
+    TwoConfigurableParams instance =
+        injector.getInstance(TwoConfigurableParams.class);
+
+    assertEquals(DOUBLE_VALUE, instance.nonConfigurableParam);
+    assertEquals(INTEGER_VALUE, instance.configurableParam1);
+    assertEquals(STRING_VALUE, instance.configurableParam2);
+  }
+
+  public void testNotEnoughValueKeysSupplied() {
+    try {
+      getInjector(
+          new LegModuleBuilder<TwoConfigurableParams>()
+            .implement(TwoConfigurableParams.class)
+            .using(INTEGER_KEY)
+            .build());
+      fail();
+    } catch (IllegalStateException expected) {}
+  }
+
+  public void testTooManyValueKeys() {
+    try {
+      getInjector(
+        new LegModuleBuilder<OneConfigurableParam>()
+            .implement(OneConfigurableParam.class)
+            .using(STRING_KEY)
+            .using(INTEGER_KEY)
+            .build());
+      fail();
+    } catch (IllegalStateException expected) {}
+  }
+
+  public void testClassHasMoreThanOneCtr() {
+    try {
+      getInjector(
+          new LegModuleBuilder<MoreThanOneConstructorMarkedWithInject>()
+            .implement(MoreThanOneConstructorMarkedWithInject.class)
+            .using(STRING_KEY)
+            .build());
+      fail();
+    } catch (ConfigurationException expected) {}
+  }
+
+  public void testChangingProviderValuesReturnSameConfiguredInstance() {
+    Module customSeedModule = new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(Double.class).toInstance(DOUBLE_VALUE);
+        bind(INTEGER_KEY)
+            .toProvider(new Provider<Integer>() {
+
+              int i = 0;
+
+              @Override
+              public Integer get() {
+                return i++;
+              }
+            })
+            .in(Singleton.class);
+      }
+    };
+
+    Injector injector = Guice.createInjector(
+        customSeedModule,
+        new LegModuleBuilder<OneConfigurableParam>()
+            .implement(OneConfigurableParam.class)
+            .using(INTEGER_KEY)
+            .build());
+
+    OneConfigurableParam instance1 =
+        injector.getInstance(OneConfigurableParam.class);
+    assertEquals(DOUBLE_VALUE, instance1.nonConfigurableParam);
+    assertEquals(0, instance1.configurableParam.intValue());
+
+    OneConfigurableParam instance2 =
+        injector.getInstance(OneConfigurableParam.class);
+    assertEquals(DOUBLE_VALUE, instance2.nonConfigurableParam);
+    assertEquals(0, instance2.configurableParam.intValue());
+  }
+
+  private <T> Injector getInjector(Module... modules) {
+    List<Module> allModules = Arrays.asList(modules);
+    allModules.add(SEED_MODULE);
+    return Guice.createInjector(allModules);
+  }
+}
