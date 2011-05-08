a8f650e191f15dfe35eb4510877d54d80fb028dc
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-05-08 23:08:15 +0000

    NestedScope clean-up.

--- inject/nestedscope/src/main/java/com/google/gimlet/inject/nestedscope/ScopeIdStackProvider.java
+++ inject/nestedscope/src/main/java/com/google/gimlet/inject/nestedscope/ScopeIdStackProvider.java
@@ -5,16 +5,15 @@ package com.google.gimlet.inject.nestedscope;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.inject.Inject;
-import com.google.inject.Key;
 import com.google.inject.Provider;
 
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Provides the "stack" of scope ids that have been accumulated via the entering
- * of scopes for the current thead. The head (beginning) of the deck is the most
- * recently added ScopeId.
+ * Provides the stack of scope ids that have been accumulated via the entering
+ * of scopes for the current thread. The head (beginning) of the deck is the
+ * most recently added scope id.
  */
 class ScopeIdStackProvider implements Provider<Iterable<ScopeId>> {
 
@@ -31,7 +30,7 @@ class ScopeIdStackProvider implements Provider<Iterable<ScopeId>> {
     for (BindingFrame bindingFrame : bindingFrameStack.getBindingFrames()) {
       ScopeId scopeId =
           checkNotNull(
-              bindingFrame.get(Key.get(ScopeId.class, ScopeIdKey.class)),
+              bindingFrame.get(BindingFrame.SCOPE_ID_KEY),
               "Bindingframe %s did not have scope key in binding stack %s",
               bindingFrame, bindingFrameStack);
       scopeIds.add(scopeId);
