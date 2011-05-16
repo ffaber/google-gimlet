c9cb647f4ebad5e9108fc95efb367614ad4f0618
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-05-16 01:56:31 +0000

    Adding abstract batching implementations.

---
+++ trunk/collect/src/main/java/com/google/gimlet/batching/ListLoadingBatchIterable.java
@@ -0,0 +1,128 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.gimlet.batching;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.AbstractIterator;
+import com.google.gimlet.parallel.CallableTransform;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Base class that makes it possible to present a flat iterable view of
+ * multiple loads that each result in their own list being loaded. Each call to
+ * get the iterable will result in a new iterators that starts at the beginning
+ * of the "batches" to load.
+ *
+ * @author veeraj
+ * @param <I> represents the type of id that are used in fetching.
+ * @param <T> represents the type of objects that being loaded.
+ */
+public abstract class ListLoadingBatchIterable<I, T>
+    implements Iterable<T> {
+
+  private final I initialValue;
+  private final CallableTransform loadingCallableTransform;
+
+  /**
+   * Constructor.
+   *
+   * @param initialValue the initial value to use when getting the first batch.
+   * @param loadingCallableTransform a transform to apply to the callable which
+   * calls {@link #loadNextBatch(Object)}  that so that one may decorate it with
+   * specific logic such as loading in a separate thread.
+   */
+  public ListLoadingBatchIterable(
+      I initialValue,
+      CallableTransform loadingCallableTransform) {
+    this.initialValue = initialValue;
+    this.loadingCallableTransform = loadingCallableTransform;
+  }
+
+  /**
+   * Returns the nextId that will used as the beginning of the next batch given
+   * the last entry returned from the current batch.
+   */
+  protected abstract I getNextId(T previousEnd);
+
+  /** Loads a batch of items starting with beginIdForBatch. */
+  protected abstract List<T> loadNextBatch(I beginIdForBatch);
+
+  @Override public final Iterator<T> iterator() {
+    return new ListLoadingIterator();
+  }
+
+  /**
+   * Helper class that allows us to return a new iterator starts the beginning
+   * for each call to get the iterator on the Iterable.
+   */
+  private class ListLoadingIterator extends AbstractIterator<T> {
+
+    /**
+     * Holds the next id that we'll use to
+     * {@link ListLoadingBatchIterable#loadNextBatch(Object)}.
+     */
+    I idForNextBatch;
+
+    /**
+     * Keeps a reference to the current batch that we're returning entries from.
+     * When this iterator gets exhausted, we'll load another batch to replace
+     * it.
+     */
+    private Iterator<T> currentBatch;
+
+    private ListLoadingIterator() {
+      idForNextBatch = Preconditions.checkNotNull(
+          initialValue, "Can't have a null initial value");
+    }
+
+    private Iterator<T> getNextBatch() {
+      checkState(idForNextBatch != null,
+          "Can't get next batch after an empty batch");
+      Callable<List<T>> loadingCallable = new Callable<List<T>>() {
+        @Override
+        public List<T> call() throws Exception {
+          return loadNextBatch(idForNextBatch);
+        }
+      };
+
+      final List<T> batch;
+      try {
+        batch = loadingCallableTransform.transform(loadingCallable).call();
+      } catch (Exception e) {
+        throw new RuntimeException(
+            "Could not load batch with id " + idForNextBatch, e);
+      }
+
+      if (!batch.isEmpty()) {
+        idForNextBatch = getNextId(batch.get(batch.size() - 1));
+      } else {
+        idForNextBatch = null;
+      }
+      return batch.iterator();
+    }
+
+    @Override protected T computeNext() {
+      if (currentBatch == null) {
+        currentBatch = getNextBatch();
+        if (!currentBatch.hasNext()) {
+          // the very first batch was empty.
+          return endOfData();
+        }
+      }
+      if (!currentBatch.hasNext()) {
+        currentBatch = getNextBatch();
+        // we exhausted one batch, and tried to get the next which had none,
+        // this means we have reached the end.
+        if (!currentBatch.hasNext()) {
+          return endOfData();
+        }
+      }
+      return currentBatch.next();
+    }
+  }
+}
---
+++ trunk/collect/src/main/java/com/google/gimlet/batching/MapLoadingBatchIterable.java
@@ -0,0 +1,133 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.gimlet.batching;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+import com.google.gimlet.parallel.CallableTransform;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Helper classes that facilitates loading {@link KeyedBatch keyed batches}.
+ * <p>
+ * This is useful when trying to load all of foo for a given bar for all bars
+ * from disk.
+ *
+ * @param <K> the type of "bar" that are being loaded.
+ * @param <V> the type of "foos" that are being loaded.
+ * @param <I> the type of id for bars so that the next id can be determined.
+ */
+public abstract class MapLoadingBatchIterable<K, V, I>
+    extends ListLoadingBatchIterable<I, KeyedBatch<K, V>> {
+
+  private static final Logger logger = 
+      Logger.getLogger(MapLoadingBatchIterable.class.getCanonicalName());
+
+  /** Function that converts a map entry to a batch element. */
+  private final Function<Entry<K, List<V>>, KeyedBatch<K, V>>
+      mapEntryToBatchElementFunction =
+      new Function<Entry<K, List<V>>, KeyedBatch<K, V>>() {
+        @Override public KeyedBatch<K, V> apply(Entry<K, List<V>> from) {
+          return KeyedBatch.of(
+              from.getKey(), Batches.forCollection(from.getValue()));
+        }
+      };
+
+  private final Integer batchSize;
+
+  /**
+   * @param initialValue an initial value to represent the first "bar" to load.
+   * @param loadCallableTransform transform that gets applied when
+   * {@link #loadNextBatch} get called within its own callable. This allows one
+   * to decorate this call.
+   * @param batchSize The max number of "foos" that get returned for a given
+   */
+  protected MapLoadingBatchIterable(
+      I initialValue,
+      CallableTransform loadCallableTransform,
+      Integer batchSize) {
+    super(initialValue, loadCallableTransform);
+    this.batchSize = batchSize;
+  }
+
+  /**
+   * Loads a batch of entries starting with the given beginIdForBatch. The sum
+   * of the sizes of values for enries should at most be batch size. One does
+   * not need to worry if the "last" K (bar) according to the ordering is
+   * incomplete or not. It will automatically be removed and the
+   * nextIdFunction will be applied to the previous element which should result
+   * in the last "K" being loaded.
+   */
+  protected abstract Map<K, List<V>> loadBatch(I beginIdForBatch,
+      Integer batchSize);
+
+  @Override protected final List<KeyedBatch<K, V>> loadNextBatch(
+      I beginIdForBatch) {
+    Map<K, List<V>> map = loadBatch(beginIdForBatch, batchSize);
+
+    // exit early if we didn't load any entries.
+    if (map.isEmpty()) {
+      return ImmutableList.of();
+    }
+
+    List<KeyedBatch<K, V>> keyedBatches =
+        Lists.newArrayList(Iterables.transform(
+            map.entrySet(), mapEntryToBatchElementFunction));
+
+    Collections.sort(keyedBatches,
+        // sort the list based in the key of the KeyedBatches.
+        getOrdering().onResultOf(
+            new Function<KeyedBatch<K, V>, K>() {
+              @Override
+              public K apply(KeyedBatch<K, V> from) {
+                return from.getKey();
+              }
+            }));
+
+    // if there is only one element, make sure that this one key doesn't
+    // have too many values. if it does log an exception. We will also just
+    // return this single element list.
+
+    if (keyedBatches.size() == 1) {
+      KeyedBatch<K, V> keyedBatch = getOnlyElement(keyedBatches);
+      if (keyedBatch.getBatch().size() >= batchSize) {
+        logger.log(Level.SEVERE, String.format(
+            "%s has gone over batch size %s", keyedBatch.getKey(), batchSize),
+            new Exception());
+      }
+      return keyedBatches;
+    }
+
+    // since there is more than one entry in the map, we remove the last entry
+    // so that we may load it again since it may have not been complete.
+    keyedBatches.remove(keyedBatches.size() - 1);
+    return keyedBatches;
+  }
+
+  /**
+   * Returns an ordering for how to sort the map keys so that we can return
+   * them in the correct order.
+   */
+  protected abstract Ordering<K> getOrdering();
+
+  /**
+   * Returns the nextId that should be use to load the next batch, based on
+   * the last key that was part of the previous batch.
+   */
+  protected abstract I innerGetNextId(K previousEnd);
+
+  @Override protected I getNextId(KeyedBatch<K, V> previousEnd) {
+    return innerGetNextId(previousEnd.getKey());
+  }
+}
