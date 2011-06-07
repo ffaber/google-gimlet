cfd76df74222b0fae3d5aab044fd73bc065859a1
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-06-07 11:27:28 +0000

    Adding introspecting scoper.

---
+++ trunk/inject/introspectingscoper/src/main/java/com/google/gimlet/inject/App.java
@@ -0,0 +1,13 @@
+package com.google.gimlet.inject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
