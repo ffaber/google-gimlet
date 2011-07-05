6ab747c420858742210856256835afea2daa1e0d
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-07-05 19:22:08 +0000

    File scrubs.

--- reflect/src/main/java/com/google/gimlet/reflect/CompareByMethodName.java
+++ reflect/src/main/java/com/google/gimlet/reflect/CompareByMethodName.java
@@ -1,4 +1,19 @@
-// Copyright 2009 Google Inc.  All Rights Reserved 
+/**
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 package com.google.gimlet.reflect;
 
@@ -9,7 +24,7 @@ import java.util.Comparator;
  * Compares methods by their names, deferring to the value of
  * {@link String#compareTo(Object)} for a return value.
  *
- * @author ffaber@google.com (Fred Faber)
+ * @author ffaber@gmail.com (Fred Faber)
  */
 class CompareByMethodName implements Comparator<Method> {
 
