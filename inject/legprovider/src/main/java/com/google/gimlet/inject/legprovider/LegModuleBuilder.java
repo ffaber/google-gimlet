bd48fd60cce9aceed488b612f18ab83554cab34b
ffaber <ffaber@df1093ae-fba6-60b4-d989-de8ea95df4e1>
2011-07-31 01:19:32 +0000

    Pointing javadoc => wiki.

--- inject/legprovider/src/main/java/com/google/gimlet/inject/legprovider/LegModuleBuilder.java
+++ inject/legprovider/src/main/java/com/google/gimlet/inject/legprovider/LegModuleBuilder.java
@@ -28,15 +28,15 @@ import com.google.inject.TypeLiteral;
 import java.lang.annotation.Annotation;
 import java.util.LinkedHashSet;
 
-// TODO(ffaber): rewrite / update javadoc to reflect new DSL
 /**
  * This class builds modules for a leg of a configurable class. There is a clear
  * conceptual overlap between this class and {@code FactoryModuleBuilder}. In
  * the same way that {@code FactoryModuleBuilder} offers factories that use
  * runtime args to configure an injectable instance, {@link LegModuleBuilder}
  * offers providers that use args available within a {@code configure()} method.
- * As such, there might be room for future integration between this package
- * and the Guice extension, {@code assistedinject}.
+ *
+ * <p>Note that in this context, "runtime" means behavior that happens after
+ * the application's injector is created.
  *
  * <p>For the use case when an implementation of an interface requires runtime
  * paramaters in order to get initialized, {@code FactoryModuleBuilder} allows
@@ -47,157 +47,9 @@ import java.util.LinkedHashSet;
  * runtime component. Instead the client is trying to set configurable
  * components of a class to a particular set of {@link Key}s.
  *
- * <p>An example is worth a thousand words. So lets take a look at one.
- *
- * <p>Lets say that we have to ways of generating strings. One simply generates
- * unique ids and the other generates slightly more clever names.
- *
- * <pre>
- * public class BoringNameProvider implements Provider<String> {
- *   int count = 0;
- *
- *   {@literal @}Override
- *   public String get() {
- *     return String.valueOf(count++);
- *   }
- * }
- *
- * public class FunNameProvider implements Provider<String> {
- *
- *   final static String[] COOL_NAMES = {
- *     "B. A. Baracus", "Dirty Harry", "James Bond"};
- *
- *   int count = 0;
- *   int[] NAME_SUFFIX = new int[COOL_NAMES.length];
- *
- *   FunNameProvider() {
- *     Arrays.fill(NAME_SUFFIX, 0);
- *   }
- *
- *   {@literal @}Override
- *   public String get() {
- *     int index = count++ % COOL_NAMES.length
- *     return COOL_NAMES[index] + COUNT[index]++;
- *   }
- * }
- * </pre>
- *
- * <p>The naming providers are bound in their own module and can be used
- * anywhere in the application.
- *
- * <pre>
- * public NamingModule extends AbstractModule {
- *   {@literal @}Override
- *   protected void configure() {
- *     bind(String.class)
- *       .annotatedWith(IsBoringName.class)
- *       .toProvider(BoringNameProvider);
- *     bind(String.class)
- *       .annotatedWith(IsFunName.class)
- *       .toProvider(FunNameProvider.class);
- *   }
- * }
- * </pre>
- *
- * <h3>Configuring with one configurable parameter</h3>
- *
- * <p>Let's consider a class with configurable components:
- *
- * <pre>
- * public interface Dog {...}
- *
- * public class DogImpl {
- *
- *   {@literal @}Inject
- *   public DogImpl(
- *     Neighborhood neighborhood,
- *     Date date,
- *     {@literal @}Leg String name) {
- *      ...
- *    }
- * }
- * </pre>
- *
- * <p>For argument's sake we'll pretend that this class is used in some
- * application that monitors the streets for dogs and assigns a name to each dog
- * that walks by. {@code Neighborhood} and {@code Date} classes have no role in
- * this example, except to demonstrate that a configurable class can easily
- * support the injection of already bound elements.
- *
- * <p>Now lets see if we can configure our {@code Dog}s to be named in different
- * ways.
- *
- * <pre>
- * public DogObservationModule extends AbstractModule {
- *   {@literal @}Override
- *   protected void configure() {
- *    LegModuleBuilder{@literal <}Dog{@literal >} legModuleBuilder =
- *      new LegModuleBuilder{@literal <}Dog{@literal >}();
- *
- *    install(
- *      legModuleBuilder
- *       .implement(Dog.class, DogImpl.class)
- *       .using(Key.get(String.class, IsBoringName.class))
- *       .build(IsBoringDog.class));
- *
- *    install(
- *      legModuleBuilder
- *       .implement(Dog.class, DogImpl.class)
- *       .using(Key.get(String.class, IsFunName.class))
- *       .build(IsFunDog.class));
- *   }
- * }
- * </pre>
- *
- * <p>We've now successfully been able to reuse the same {@code Dog} class for
- * two different naming schemes.
- *
- * <h3>Configuring multiple with multiple configurable parameters</h3>
- *
- * <p>Let's slightly change the example. Suppose that we would like each dog to
- * have both names associated with it. In other words, our implementation class
- * would look something like this.
- *
- * <pre>
- * public interface Dog {...}
- *
- * public class DogImpl {
- *
- *   {@literal @}Inject
- *   public DogImpl(
- *     Neighborhood neighborhood,
- *     Date date,
- *     {@literal @}Leg("one") String name1,
- *     {@literal @}Leg("two") String name2) {
- *      ...
- *    }
- * }
- * </pre>
- *
- * <p>For the sake of convenience lets say that we would like to have one of the
- * names to be a boring id and the other one to be a slightly more fun name,
- * then we could use the following configuration.
- *
- * <pre>
- * public DogObservationModule extends AbstractModule {
- *   {@literal @}Override
- *   protected void configure() {
- *    LegModuleBuilder{@literal <}Dog{@literal >} legModuleBuilder =
- *      new LegModuleBuilder{@literal <}Dog{@literal >}();
- *
- *    install(
- *      legModuleBuilder
- *       .implement(Dog.class, DogImpl.class)
- *       .using(Key.get(String.class, IsBoringName.class), "one")
- *       .using(Key.get(String.class, IsFunName.class), "two")
- *       .build());
- *   }
- * }
- * </pre>
- *
- * <p>Notice that this time we didn't provide any annotation to the build
- * method. This means that we are configuring an annotated version of
- * {@code Dog}.
+ * <p>An example is worth a thousand words. Please have a look at the
+ * <a href="https://code.google.com/p/google-gimlet/wiki/GimletLegProvider">
+ * leg provider wiki</a> page for an illustration.
  *
  */
 public class LegModuleBuilder {
